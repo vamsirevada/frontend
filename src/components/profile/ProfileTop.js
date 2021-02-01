3d8926ec0246a77f566052c70edf1b44f759bc9b
ketu-arun <arun.08031992@gmail.com>
2021-02-02 01:14:10 +0530

    view profile sorted

--- src/components/profile/ProfileTop.js
+++ src/components/profile/ProfileTop.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import gps from '../../images/icons/noun_Location_3139654.svg';
-=======
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Fragment } from "react";
-import PropTypes from "prop-types";
-import gps from "../../images/icons/noun_Location_3139654.svg";
->>>>>>> 9d08fede18260942a8410a3ee91ea8c40531ffc5
 
 const ProfileTop = ({
   profile: { user, buddies, location, status, experience, avatar },
@@ -20,7 +14,6 @@ const ProfileTop = ({
             <div className='display-pic'>
               <img className='display-pic' src={avatar} alt='' />
             </div>
-<<<<<<< HEAD
             <div className='profile-info-box'>
               <a href='#!'>
                 <p className='border-1 white'>
@@ -32,19 +25,6 @@ const ProfileTop = ({
                 <p className='white'>
                   <span className='f-1'>{experience && experience.length}</span>
                   <br /> Projects{' '}
-=======
-            <div className="profile-info-box">
-              <a>
-                <p className="border-1">
-                  <span className="f-1">{buddies && buddies.length}</span>
-                  <br /> Connections
-                </p>
-              </a>
-              <a>
-                <p>
-                  <span className="f-1">{experience && experience.length}</span>
-                  <br /> Projects{" "}
->>>>>>> 9d08fede18260942a8410a3ee91ea8c40531ffc5
                 </p>
               </a>
             </div>
