97f867651d581b998241abf0c2de3c2ee9048206
ketu-arun <arun.08031992@gmail.com>
2021-03-05 09:55:42 +0530

    addmemeber css

--- src/components/projects/AddPopUp.js
+++ src/components/projects/AddPopUp.js
@@ -1,15 +1,32 @@
 import React, {
   Fragment,
   useState,
+  useContext,
   forwardRef,
   useImperativeHandle,
 } from 'react';
 import nounPlus from '../../images/icons/noun_Plus_2310779.svg';
 import logo from '../../images/dummyimage.jpg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import searchIcon from '../../images/searchIcon.svg';
+import { SearchContext } from '../../context/search.context';
+import axios from 'axios';
 
 const AddPopUp = forwardRef(({}, ref) => {
+  const history = useHistory();
   const [boxIsOpen, setBoxIsOpen] = useState(true);
+  const { Addsearch, clearSearch } = useContext(SearchContext);
+
+  const _onsearch = async (value) => {
+    clearSearch();
+    const response = await axios.get(`api/search?title=${value}`);
+    console.log(response, 'responseresponseresponse');
+    if (response) {
+      Addsearch(response?.data);
+    } else {
+      Addsearch([]);
+    }
+  };
 
   const handleOpen = () => {
     setBoxIsOpen(true);
@@ -37,6 +54,26 @@ const AddPopUp = forwardRef(({}, ref) => {
                 <a href='#!' className='member-cross' onClick={handleClose}>
                   <img src={nounPlus} alt='' />
                 </a>
+              </div>
+              <div className='search active'>
+                <input
+                  type='text'
+                  onChange={(e) => {
+                    setTimeout(() => {
+                      _onsearch(e.target.value);
+                    }, 500);
+                  }}
+                  className='search-btn'
+                  placeholder='search'
+                />
+                <br />
+                <img
+                  onClick={() => {
+                    history.push('/profiles');
+                  }}
+                  src={searchIcon}
+                  alt='search'
+                />
               </div>
               {/* {members.length > 0 ? (
                 <Fragment>
