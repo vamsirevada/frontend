7b109874b84c07e561557529a93e90ac6a702cf2
vamsirevada <vamsirevada4444@gmail.com>
2021-03-20 20:54:20 +0530

    notice board popup

--- src/components/projects/NoticeBoardPopup.js
+++ src/components/projects/NoticeBoardPopup.js
@@ -1,15 +1,27 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { applyNotice } from '../../actions/notice';
 import Moment from 'react-moment';
 
 import nounPlus from '../../images/icons/noun_Plus_2310779.svg';
+import Spinner from '../layout/Spinner';
 
 const NoticeBoardPopup = ({ notice, applyNotice, hide }) => {
+  const [loading, setLoading] = useState(true);
+
   const close = () => {
     hide();
   };
+
+  useEffect(() => {
+    const t = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+    return () => {
+      clearTimeout(t);
+    };
+  });
 
   const apply = () => {
     applyNotice(notice?._id);
@@ -17,65 +29,73 @@ const NoticeBoardPopup = ({ notice, applyNotice, hide }) => {
   };
 
   return (
-    <div className='noticeboardpopup'>
-      <div className='noticeboardpopup-heading'>
-        <h3>{notice?.title}</h3>
-        <a className='noticeboardpopup-cross' onClick={close}>
-          <img src={nounPlus} alt='' />
-        </a>
-      </div>
-      <div className='noticeboardpopup-main'>
-        <div className='noticeboardpopup-content'>
-          <h5>Posted by :</h5>
-          <span>
-            <p>{notice?.project?.projectname}</p>
-          </span>
+    <>
+      {loading ? (
+        <div className='noticeboardpopup'>
+          <Spinner />
         </div>
-        <div className='noticeboardpopup-content'>
-          <h5>Posted on : </h5>{' '}
-          <span>
-            <p>
-              <Moment format='Do MMMM'>{notice?.date}</Moment>
-            </p>
-          </span>
+      ) : (
+        <div className='noticeboardpopup'>
+          <div className='noticeboardpopup-heading'>
+            <h3>{notice?.title}</h3>
+            <a className='noticeboardpopup-cross' onClick={close}>
+              <img src={nounPlus} alt='' />
+            </a>
+          </div>
+          <div className='noticeboardpopup-main'>
+            <div className='noticeboardpopup-content'>
+              <h5>Posted by :</h5>
+              <span>
+                <p>{notice?.project?.projectname}</p>
+              </span>
+            </div>
+            <div className='noticeboardpopup-content'>
+              <h5>Posted on : </h5>{' '}
+              <span>
+                <p>
+                  <Moment format='Do MMMM'>{notice?.date}</Moment>
+                </p>
+              </span>
+            </div>
+            <div className='noticeboardpopup-content'>
+              <h5>Deadline : </h5>{' '}
+              <span>
+                <p>
+                  <Moment format='Do MMMM'>{notice?.deadline}</Moment>
+                </p>
+              </span>
+            </div>
+            <div className='noticeboardpopup-content'>
+              <h5>Eligibility : </h5>{' '}
+              <span>
+                <p>{notice?.eligibility}</p>
+              </span>
+            </div>
+            <div className='noticeboardpopup-content'>
+              <h5>Venue : </h5>{' '}
+              <span>
+                <p>{notice?.venue}</p>
+              </span>
+            </div>
+            <div className='noticeboardpopup-content'>
+              <h5>Role : </h5>{' '}
+              <span>
+                <p>{notice?.role}</p>
+              </span>
+            </div>
+            <div className='noticeboardpopup-content'>
+              <h5>Description :</h5>
+              <p>{notice?.description}</p>
+            </div>
+          </div>
+          <div>
+            <button onClick={apply} className='btn-blue'>
+              Apply
+            </button>
+          </div>
         </div>
-        <div className='noticeboardpopup-content'>
-          <h5>Deadline : </h5>{' '}
-          <span>
-            <p>
-              <Moment format='Do MMMM'>{notice?.deadline}</Moment>
-            </p>
-          </span>
-        </div>
-        <div className='noticeboardpopup-content'>
-          <h5>Eligibility : </h5>{' '}
-          <span>
-            <p>{notice?.eligibility}</p>
-          </span>
-        </div>
-        <div className='noticeboardpopup-content'>
-          <h5>Venue : </h5>{' '}
-          <span>
-            <p>{notice?.venue}</p>
-          </span>
-        </div>
-        <div className='noticeboardpopup-content'>
-          <h5>Role : </h5>{' '}
-          <span>
-            <p>{notice?.role}</p>
-          </span>
-        </div>
-        <div className='noticeboardpopup-content'>
-          <h5>Description :</h5>
-          <p>{notice?.description}</p>
-        </div>
-      </div>
-      <div>
-        <button onClick={apply} className='btn-blue'>
-          Apply
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
