830c09d44a438e2cb04392ec3b0a5392f644f514
ketu-arun <arun.08031992@gmail.com>
2021-03-30 19:13:58 +0530

    transistion started

--- src/components/layout/landing pages/Page6.js
+++ src/components/layout/landing pages/Page6.js
@@ -1,28 +1,35 @@
-import React, { Fragment } from "react";
-import manage from "../../../images/manage.png";
+import React, { Fragment } from 'react';
+import manage from '../../../images/manage.png';
 
 const Page6 = () => {
   return (
     <Fragment>
-      <div id="page-6" className="page6-columns page">
-        <div className="container">
-          <div className="column">
-            <div className="column-1">
-              <img className="collobrate" src={manage} alt="collobrate" />
+      <div
+        id='page-6'
+        className='page6-columns page'
+        data-aos='fade'
+        data-aos-delay='25'
+        data-aos-duration='500'
+        data-aos-easing='ease'
+      >
+        <div className='container'>
+          <div className='column'>
+            <div className='column-1'>
+              <img className='collobrate' src={manage} alt='collobrate' />
               <br />
             </div>
-            <div className="column-2">
-              <h1 className="ft-heading ft-heading-light">
+            <div className='column-2'>
+              <h1 className='ft-heading ft-heading-light'>
                 Easily manage your projects
               </h1>
-              <p className="ft-para ft-para-light">
+              <p className='ft-para ft-para-light'>
                 Manage your projects and reduce the cost, energy and effort for
                 processes like searching talent, auditioning and hiring talents
                 across India, effectively increasing your productivity &
                 efficiency.
               </p>
               <br />
-              <h3 className="ft-heading ft-heading-light">Coming Soon...</h3>
+              <h3 className='ft-heading ft-heading-light'>Coming Soon...</h3>
             </div>
           </div>
         </div>
