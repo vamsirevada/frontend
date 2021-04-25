24d279bd13573bf65df12b174044d2e55954faee
vamsirevada <vamsirevada4444@gmail.com>
2021-04-25 20:05:20 +0530

    updating...

--- src/components/addportfolio/Modal.js
+++ src/components/addportfolio/Modal.js
@@ -185,11 +185,13 @@ const Modal = ({
                         by <span className='blue'>{user.fullName}</span>
                         {', '}
                         <Moment format='DD MMM YY'>
-                          {portfolio?.createdAt.toDate()}
+                          {portfolio?.createdAt &&
+                            portfolio?.createdAt.toDate()}
                         </Moment>{' '}
                         {', '}
                         <Moment format='hh:mm A'>
-                          {portfolio?.createdAt.toDate()}
+                          {portfolio?.createdAt &&
+                            portfolio?.createdAt.toDate()}
                         </Moment>
                       </p>
                     </div>
